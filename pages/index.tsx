5d804a2efcb0706470a5096b58b8ba3c2c6fcbe6
Martin Claudel <yornclaudel@gmail.com>
2022-10-10 12:24:52 +0200

    added temp construction svg, working on some animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,50 @@
 import { copyFileSync } from "fs";
 import type { NextPage, NextComponentType } from "next";
-import { useEffect, useState } from "react";
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
-import styles from "../styles/home.module.css";
+import Circle from "../components/Circle";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Home: NextPage = () => {
-  function transcos(offset: number) {
-    return Math.cos(offset);
-  }
-
-  const [offsets, setOffsets] = useState([
-    { x: 0, y: 0 },
-    { x: 0, y: 0 },
-    { x: 0, y: 0 },
-  ]);
-  let currentFrame: number;
-
-  const animate = () => {
-    setOffsets((arr: { x: number; y: number }[]) => {
-      let newArr = [...arr];
-      for (let i = 0; i < newArr.length; i++) {
-        newArr[i].x = Math.cos(i + Date.now() / 1000) * 100 + 50 * (i + 1);
-        newArr[i].y = Math.sin(i + Date.now() / 1000) * 100 + 50 * (i + 1);
-      }
-      return newArr;
-    });
-    currentFrame = requestAnimationFrame(animate);
-  };
-  useEffect(() => {
-    animate();
-    return () => cancelAnimationFrame(currentFrame);
-  }, []);
-
   return (
     <div className=" flex flex-col items-center">
-      <span className="text-sm">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        egestas urna justo, id cursus ex iaculis sit amet. Morbi sagittis nisi
-        vitae tellus pulvinar semper. Fusce tempus diam quis ex maximus, ut
-        imperdiet enim pulvinar. Curabitur lobortis sed tellus a ornare. Nulla
-        malesuada ac elit at accumsan. Vestibulum feugiat nisi nec sem mollis,
-        et porttitor ex tempus. Mauris in ex at dui blandit maximus. Mauris sed
-        blandit augue.
-      </span>
-      <div className={`${styles.animatedCircle} text-3xl`}>
-        <FaGithub
-          style={{
-            transform: `translate(${offsets[0].x}px, ${offsets[0].y}px)`,
-          }}
-        />
-        <FaLinkedin
-          style={{
-            transform: `translate(${offsets[1].x}px, ${offsets[1].y}px)`,
-          }}
-        />
-        <FaTwitter
-          style={{
-            transform: `translate(${offsets[2].x}px, ${offsets[2].y}px)`,
-          }}
-        />
+      <p align="justify" className="text-base my-5">
+        Hello, my name is Martin. I study Computer Science at{" "}
+        <a
+          href="https://42.fr"
+          className="underline ml-2 font-black decoration-4 decoration-indigo-700"
+        >
+          42 Paris
+        </a>
+        . You can check out my projects over on my
+        <a
+          href="https://github.com/yorncl/"
+          className="underline ml-2 font-black decoration-4 decoration-pink-700"
+        >
+          Github
+        </a>
+        .
+      </p>
+      <div className="flex flex-col mt-4 text-xl items-start">
+        <a href="https://github.com/yorncl/">
+          <FaGithub className="inline text-3xl" />
+          <span>Github</span>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/martin-claudel-b50362150/"
+          className="mt-3"
+        >
+          <FaLinkedin className="inline text-3xl" />
+          <span>Linkedin</span>
+        </a>
       </div>
+      <img className="mt-10" src="/construction.svg" alt="construction sign" />
+      <div className="flex flex-col items-center justify-start">
+        <h2 className="font-black text-center">Under construction</h2>
+        <span className="text-xs text-center">
+          Later, you might find a semi-informative blog, or a cool SVG
+          aninmation. Who knows ?
+        </span>
+      </div>
+      {/* <Circle></Circle> */}
     </div>
   );
 };
